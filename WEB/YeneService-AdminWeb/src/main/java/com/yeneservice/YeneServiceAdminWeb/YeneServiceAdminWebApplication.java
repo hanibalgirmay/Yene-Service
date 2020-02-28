9cb2b586215bf41edae1e6e6112770bc5098c65c
Hanibal Girmay <hanibalgirmay@gmail.com>
2020-02-28 10:52:55 +0300

    first commit

---
+++ WEB/YeneService-AdminWeb/src/main/java/com/yeneservice/YeneServiceAdminWeb/YeneServiceAdminWebApplication.java
@@ -0,0 +1,13 @@
+package com.yeneservice.YeneServiceAdminWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeneServiceAdminWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeneServiceAdminWebApplication.class, args);
+	}
+
+}
